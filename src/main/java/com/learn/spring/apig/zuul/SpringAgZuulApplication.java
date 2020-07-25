53cf719a081c14776a49d42c1bfd7f999487c198
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-25 13:06:22 +0530

    Boiler Plate Code Added Closes#1

---
+++ src/main/java/com/learn/spring/apig/zuul/SpringAgZuulApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.apig.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAgZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAgZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/spring/apig/zuul/SpringAgZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.apig.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAgZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
